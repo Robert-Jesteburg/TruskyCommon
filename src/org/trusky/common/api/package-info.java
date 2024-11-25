1a4cafd3b5680dd8dfebcdcc3847cc71d9425bd1
robert <robert-niemann@gmx.net>
2024-11-25 16:38:07 +0100

    Package info created.

---
+++ src/org/trusky/common/api/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * API bundle for the library.
+ *
+ * <p>All interfaces herein (including the subfolders) are considered official and can be used in any other product.</p>
+ */
+package org.trusky.common.api;
