7954a4372dea0c18f09581c49b8755aee7fa751f
robert <robert-niemann@gmx.net>
2025-03-15 13:17:26 +0100

    StartParameterManager created for use with the different Application implementations.

--- src/org/trusky/common/api/startparameters/StartOption.java
+++ src/org/trusky/common/api/startparameters/StartOption.java
@@ -11,55 +11,58 @@ import java.util.Optional;
  * Each start option has a name (like --configDir) that denotes the beginning and can be used to find the correct
  * Option class; there may or may not a value following
  * (in the case of --configDir there will be one: The directory base name).
+ *
+ * @param <DEFAULT_VAL_TYPE> The type of the default value (mostly Sting)
+ * @param <SUB_VAL_TYPE>     The type of sub value types (mostly String)
  */
 public interface StartOption<DEFAULT_VAL_TYPE extends OptionValue<?>, SUB_VAL_TYPE extends OptionValue<?>> {
 
-	public String getOptionName();
+	String getOptionName();
 
 	/**
 	 * Options itself can act as switches (like --use-strict-syntax) in which case they do not have a value.
 	 *
 	 * @return see above
 	 */
-	public boolean isDefaultValuePresent();
+	boolean isDefaultValuePresent();
 
 	/**
 	 * Get the default value or empty, if not specified on the command line.
 	 *
 	 * @return s.a.
 	 */
-	public Optional<DEFAULT_VAL_TYPE> getDefaultValue();
+	Optional<DEFAULT_VAL_TYPE> getDefaultValue();
 
 	/**
 	 * Builds an array with all the sub option names that were supplied on the actual command line. Those names can
 	 * then be used to access the value.
 	 *
-	 * @return
+	 * @return The default value
 	 */
-	public String[] getSubOptionNames();
+	String[] getSubOptionNames();
 
 	/**
 	 * An option may be present, but with no value following it (may needs none, if actiong like a switch for example)
 	 *
 	 * @return s.a.
 	 */
-	public boolean isSubOptionPresent(String subOptionName);
+	boolean isSubOptionPresent(String subOptionName);
 
 	/**
 	 * If the option is present this method tells if there is a value for present.
 	 *
-	 * @param subOptionName
+	 * @param subOptionName Name of the sub option
 	 * @return s.a.
 	 */
-	public boolean hasValue(String subOptionName);
+	boolean hasValue(String subOptionName);
 
 	/**
 	 * Get the list of values from the sub option or empty if either the sub option wasn't specified at all or if
 	 * no value was
 	 * supplied on the command line.
 	 *
-	 * @param subOptionName
+	 * @param subOptionName Name of the sub option
 	 * @return s.a.
 	 */
-	public List<SUB_VAL_TYPE> getValue(String subOptionName);
+	List<SUB_VAL_TYPE> getValue(String subOptionName);
 }
